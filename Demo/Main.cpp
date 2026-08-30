6e28635b7ebf4fad6076f0482db8d3c80dc0de35
voidProc <voidproc@gmail.com>
2026-08-30 11:10:18 +0900

    Initial commit

---
+++ Demo/Main.cpp
@@ -0,0 +1,114 @@
+﻿# include <Siv3D.hpp> // Siv3D v0.6.16
+
+struct TileInfo
+{
+	Texture faceTexture;
+};
+
+const Array<String> TileNameList = {
+	U"character_1",
+	U"character_2",
+	U"character_3",
+	U"character_4",
+	U"character_5",
+	U"character_6",
+	U"character_7",
+	U"character_8",
+	U"character_9",
+	U"circle_1",
+	U"circle_2",
+	U"circle_3",
+	U"circle_4",
+	U"circle_5",
+	U"circle_6",
+	U"circle_7",
+	U"circle_8",
+	U"circle_9",
+	U"bamboo_1",
+	U"bamboo_2",
+	U"bamboo_3",
+	U"bamboo_4",
+	U"bamboo_5",
+	U"bamboo_6",
+	U"bamboo_7",
+	U"bamboo_8",
+	U"bamboo_9",
+	U"wind_east",
+	U"wind_south",
+	U"wind_west",
+	U"wind_north",
+	U"dragon_white",
+	U"dragon_green",
+	U"dragon_red",
+};
+
+constexpr std::array<Color, 3> TileLayerColors = {
+	ColorF{ 0.2, 0.4, 1 }.lerp(Palette::Black, 0.6),
+	Palette::Green.lerp(Palette::Black, 0.3),
+	Palette::Darkred
+};
+
+constexpr Size TileFaceImageSize{ 256, 373 };
+
+void DrawTileAt(const Vec2& pos, const SizeF& tileSize, const TileInfo& tileInfo)
+{
+	const Color TileFaceColor = Palette::White.lerp(Palette::Wheat, 0.1);
+	const Color TileBorderColor = Palette::Black;
+	const double TileBorderWidth = 4.0;
+	const double TileBorderRadius = 8.0;
+	const double TileFaceScale = 0.90;
+
+	const auto scaledTexture = tileInfo.faceTexture.resized(tileSize);
+	scaledTexture.regionAt(pos).stretched(0, 0, 24, 0).rounded(TileBorderRadius).draw(Palette::Orange).drawFrame(TileBorderWidth, TileBorderColor);
+	scaledTexture.regionAt(pos).stretched(0, 0, 12, 0).rounded(TileBorderRadius).draw(Palette::Wheat.lerp(Palette::Gray, 0.3));
+	scaledTexture.regionAt(pos).rounded(TileBorderRadius).draw(TileFaceColor).drawFrame(TileBorderWidth, Palette::Wheat);
+	scaledTexture.regionAt(pos).stretched(0, 0, 24, 0).rounded(TileBorderRadius).drawFrame(TileBorderWidth, TileBorderColor);
+	scaledTexture.scaled(TileFaceScale).drawAt(pos);
+}
+
+void Main()
+{
+	Scene::Resize(1920, 1080);
+	Scene::SetResizeMode(ResizeMode::Keep);
+	Window::Resize((Scene::Size() * 0.75).asPoint());
+
+	Scene::SetBackground(ColorF{ 0, 0.4, 0.25 });
+
+	Array<TileInfo> tileInfoList;
+
+	for (const auto name : TileNameList)
+	{
+		Image image{ TileFaceImageSize, ColorF{ 0, 0 } };
+
+		for (int iLayer = 0; iLayer < 3; ++iLayer)
+		{
+			const FilePath path = U"image/tiles_{}/{}.png"_fmt(iLayer + 1, name);
+
+			if (not FileSystem::Exists(path)) continue;
+
+			Image{ path }.negated().stamp(image, 0, 0, TileLayerColors[iLayer]);
+		}
+
+		tileInfoList << TileInfo{ .faceTexture = Texture{ image } };
+	}
+
+	while (System::Update())
+	{
+		{
+			const int columns = 9;
+			const int rows = tileInfoList.size() / columns + 1;
+			const double tileWidth = 128;
+			const SizeF tileSize{ tileWidth, TileFaceImageSize.y * tileWidth / TileFaceImageSize.x };
+			const SizeF margin{ 16, 48 };
+
+			const SizeF drawAreaSize{ tileSize.x * columns + margin.x * (columns - 1), tileSize.y * rows + margin.y * (rows - 1) + margin.y / 2 };
+			Transformer2D translate{ Mat3x2::Translate((Scene::Size() - drawAreaSize) / 2) };
+
+			for (const auto [index, tileInfo] : Indexed(tileInfoList))
+			{
+				const Vec2 pos{ (index % columns) * (tileSize.x + margin.x), (index / columns) * (tileSize.y + margin.y) };
+				DrawTileAt(pos + tileSize / 2, tileSize, tileInfo);
+			}
+		}
+	}
+}
---
+++ MahjongTileExporter/Main.cpp
@@ -0,0 +1,89 @@
+﻿# include <Siv3D.hpp> // Siv3D v0.6.16
+
+/*
+ * 麻雀牌画像を保存する
+ *
+ * 使用フォント: GL-MahjongTile (https://github.com/Gutenberg-Labo/DingbatFonts)
+ */
+
+namespace
+{
+	constexpr Size SceneSize{ 1600, 1200 };
+	constexpr Size WindowSize{ 1600, 1200 };
+
+	// 出力する画像の横幅を固定
+	constexpr int32 ImageWidth = 256;
+
+	// 縦横比（適当）から画像の高さを決める
+	constexpr Size ImageSize{ ImageWidth, static_cast<int32>(ImageWidth * 1.46) };
+
+	// 枠を描画したくないので、枠が上手く見切れるようにフォントサイズを設定
+	constexpr int32 FontSize = static_cast<int32>(ImageWidth * 1.97);
+}
+
+void Main()
+{
+	Scene::SetBackground(Palette::White);
+
+	// テスト描画用シーンとウィンドウ
+	Scene::Resize(SceneSize);
+	Scene::SetResizeMode(ResizeMode::Keep);
+	Window::Resize(WindowSize);
+
+	const Font fontMahjong{ FontSize, U"font/GL-MahjongTile.ttf" };
+
+	const Array<std::pair<String, String>> tilesAll = {
+		{ U"1234", U"wind" },
+		{ U"567", U"dragon" },
+		{ U"qwertyuio", U"character" },
+		{ U"asdfghjkl", U"bamboo" },
+		{ U"zxcvbnm,.", U"circle" },
+	};
+
+	Array<Texture> textures;
+
+	// 画像を保存
+	for (const auto& tiles : tilesAll)
+	{
+		for (const auto [index, tile] : Indexed(tiles.first))
+		{
+			constexpr Size TempImageSize{ FontSize, FontSize };
+			Image image = Image{ TempImageSize, ColorF{ 0.0, 0.0 } }.clipped((TempImageSize - ImageSize) / 2, ImageSize);
+
+			fontMahjong(tile)
+				.stampAt(image, image.size() / 2, Palette::Black);
+
+			textures << Texture{ image };
+
+			const FilePath path = U"output/{}_{}.png"_fmt(tiles.second, index + 1);
+			image.savePNG(path);
+		}
+	}
+
+	while (System::Update())
+	{
+		// 切り出す領域を描画
+		{
+			Transformer2D translate{ Mat3x2::Translate(64, 64).scaled(0.5) };
+			const auto region = fontMahjong(U"1").draw(0, 0, Palette::Black);
+			RectF{ Arg::center = region.center(), ImageSize }.drawFrame(1.0, Palette::Magenta);
+		}
+
+		// 出力した画像を描画
+		{
+			Transformer2D translate{ Mat3x2::Translate(480, 64).scaled(0.5) };
+
+			for (auto [index, texture] : Indexed(textures))
+			{
+				const int cols = 8;
+				const int x = index % cols;
+				const int y = index / cols;
+
+				const auto region = texture
+					.draw(x * (ImageSize.x + 16), y * (ImageSize.y + 16), ColorF{ 1.0 });
+
+				region.drawFrame(4.0, Palette::Black);
+			}
+		}
+	}
+}
